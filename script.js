3164ca432ef6b91d03992ede51d358e7cf0b6a98
Mattias Fatzler <mattythefatty55@gmail.com>
2024-11-21 22:09:06 -0500

    random stuff that don't work but oh well

--- script.js
+++ script.js
@@ -1,95 +1,94 @@
 class Flashcard {
-    constructor(question, answer) {
-        this.question = question;
-        this.answer = answer;
-        this.isFlipped = false;
-    }
+  constructor(question, answer) {
+    this.question = question;
+    this.answer = answer;
+    this.isFlipped = false;
+  }
 }
 
 class FlashcardSystem {
-    constructor() {
-        this.flashcards = [];
-        this.currentIndex = 0;
-    }
+  constructor() {
+    this.flashcards = [];
+    this.currentIndex = 0;
+  }
 
-    addFlashcard(question, answer) {
-        const flashcard = new Flashcard(question, answer);
-        this.flashcards.push(flashcard);
-    }
+  addFlashcard(question, answer) {
+    const flashcard = new Flashcard(question, answer);
+    this.flashcards.push(flashcard);
+  }
 
-    removeFlashcard(index) {
-        if (index >= 0 && index < this.flashcards.length) {
-            this.flashcards.splice(index, 1);
-        }
+  removeFlashcard(index) {
+    if (index >= 0 && index < this.flashcards.length) {
+      this.flashcards.splice(index, 1);
     }
+  }
 
-    displayFlashcard(index) {
-        if (index >= 0 && index < this.flashcards.length) {
-            const flashcard = this.flashcards[index];
-            const flashcardContainer = document.getElementById('flashcard');
-            flashcardContainer.innerHTML = `
-                <div class="flashcard ${flashcard.isFlipped ? 'flipped' : ''}">
+  displayFlashcard(index) {
+    if (index >= 0 && index < this.flashcards.length) {
+      const flashcard = this.flashcards[index];
+      const flashcardContainer = document.getElementById("flashcard");
+      flashcardContainer.innerHTML = `
+                <div class="flashcard ${flashcard.isFlipped ? "flipped" : ""}">
                     <p class="front">${flashcard.question}</p>
                     <p class="back">${flashcard.answer}</p>
                 </div>
             `;
-        }
     }
+  }
 
-    nextFlashcard() {
-        if (this.currentIndex < this.flashcards.length - 1) {
-            this.currentIndex++;
-            this.displayFlashcard(this.currentIndex);
-        }
+  nextFlashcard() {
+    if (this.currentIndex < this.flashcards.length - 1) {
+      this.currentIndex++;
+      this.displayFlashcard(this.currentIndex);
     }
+  }
 
-    prevFlashcard() {
-        if (this.currentIndex > 0) {
-            this.currentIndex--;
-            this.displayFlashcard(this.currentIndex);
-        }
+  prevFlashcard() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      this.displayFlashcard(this.currentIndex);
     }
+  }
 
-    flipFlashcard() {
-        const flashcard = this.flashcards[this.currentIndex];
-        flashcard.isFlipped = !flashcard.isFlipped;
-        this.displayFlashcard(this.currentIndex);
-    }
+  flipFlashcard() {
+    const flashcard = this.flashcards[this.currentIndex];
+    flashcard.isFlipped = !flashcard.isFlipped;
+    this.displayFlashcard(this.currentIndex);
+  }
 }
 
 const flashcardSystem = new FlashcardSystem();
-flashcardSystem.addFlashcard('What is the capital of France?', 'Paris');
-flashcardSystem.addFlashcard('What is 2 + 2?', '4');
+flashcardSystem.addFlashcard("What is the capital of France?", "Paris");
+flashcardSystem.addFlashcard("What is 2 + 2?", "4");
 flashcardSystem.displayFlashcard(0);
 
-document.getElementById('next-btn').addEventListener('click', () => {
-    flashcardSystem.nextFlashcard();
+document.getElementById("next-btn").addEventListener("click", () => {
+  flashcardSystem.nextFlashcard();
 });
 
-document.getElementById('add-btn').addEventListener('click', () => {
-    const question = document.getElementById('question-input').value;
-    const answer = document.getElementById('answer-input').value;
-    flashcardSystem.addFlashcard(question, answer);
-    flashcardSystem.displayFlashcard(flashcardSystem.flashcards.length -1);
-    document.getElementById('question-input').value = '';
-    document.getElementById('answer-input').value = '';
-
+document.getElementById("add-btn").addEventListener("click", () => {
+  const question = document.getElementById("question-input").value;
+  const answer = document.getElementById("answer-input").value;
+  flashcardSystem.addFlashcard(question, answer);
+  flashcardSystem.displayFlashcard(flashcardSystem.flashcards.length - 1);
+  document.getElementById("question-input").value = "";
+  document.getElementById("answer-input").value = "";
 });
 
-document.getElementById('prev-btn').addEventListener('click', () => {
-    flashcardSystem.prevFlashcard();
+document.getElementById("prev-btn").addEventListener("click", () => {
+  flashcardSystem.prevFlashcard();
 });
 
-document.getElementById('flashcard').addEventListener('click', () => {
+document.getElementById("flashcard").addEventListener("click", () => {
+  flashcardSystem.flipFlashcard();
+});
+
+document.getElementById("flashcard").addEventListener("keydown", (event) => {
+  if (event.code === "Space") {
     flashcardSystem.flipFlashcard();
-});
-
-document.getElementById('flashcard').addEventListener('keydown', (event) => {
-    if (event.code === 'Space') {
-        flashcardSystem.flipFlashcard();
-    } else if (event.code === 'ArrowLeft' || event.code === 'KeyA') {
-        flashcardSystem.prevFlashcard();
-    } else if (event.code === 'ArrowRight' || event.code === 'KeyD') {
-        flashcardSystem.nextFlashcard();
-    }
+  } else if (event.code === "ArrowLeft" || event.code === "KeyA") {
+    flashcardSystem.prevFlashcard();
+  } else if (event.code === "ArrowRight" || event.code === "KeyD") {
+    flashcardSystem.nextFlashcard();
+  }
 });
